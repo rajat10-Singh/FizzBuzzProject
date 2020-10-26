80983dbc24da6bd793144ca52b7f245c20861d24
Rajat Singh <rajat10-Singh@github.com>
2020-10-26 10:13:14 +0530

    Created FizzBuzz main class and check() method from FizzBuzzTests class. Failure of case for number 1.

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,5 @@
+public class FizzBuzz {
+    public String check(int number) {
+        return "";
+    }
+}
--- src/test/java/FizzBuzzTests.java
+++ src/test/java/FizzBuzzTests.java
@@ -1,7 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FizzBuzzTests {
 
     // Write a program that prints the numbers from 1 to 100.
     // But for multiples of three print “Fizz” instead of the number
     // and for the multiples of five print “Buzz”.
     // For numbers which are multiples of both three and five print “FizzBuzz”
+
+    @Test
+    public void testNumbers(){
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        int number = 1;
+        String num = fizzbuzz.check(number);
+        Assert.assertEquals("1", num);
+    }
 }
